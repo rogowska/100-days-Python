42b1eecc9a84fef2f532191959cf1d4ecb4601ee
rogowska <rogowska.oliwia@gmail.com>
2023-11-29 18:21:49 +0100

    day 5 solution

---
+++ Day 5/passwordGenerator.py
@@ -0,0 +1,33 @@
+import random
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+           'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+           'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters = int(input("How many letters would you like in your password?\n"))
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+password = []
+
+pairs = [(nr_numbers, numbers), (nr_letters, letters), (nr_symbols, symbols)]
+
+for pair in pairs:
+    for i in range(pair[0]):
+        password.append(pair[1][random.randint(0, len(pair[1]) - 1)])
+
+
+#####easier attempt#####
+#for i in range(nr_numbers):
+#    password.append(numbers[random.randint(0, len(numbers) - 1)])
+#for i in range(nr_letters):
+#    password.append(letters[random.randint(0, len(letters) - 1)])
+#for i in range(nr_symbols):
+#    password.append(symbols[random.randint(0, len(symbols) - 1)])
+
+random.shuffle(password)
+password = ''.join(password)
+print('Your password is:', password)
