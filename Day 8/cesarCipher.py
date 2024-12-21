8e70acad8bf7c8c63743c2bf3d17e88017bf0af4
rogowska <rogowska.oliwia@gmail.com>
2024-12-21 22:50:29 +0100

    chamges for cesarCipher

--- Day 8/cesarCipher.py
+++ Day 8/cesarCipher.py
@@ -3,6 +3,7 @@ from art import logo
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
             'v', 'w', 'x', 'y', 'z']
 
+
 def encrypt(text, shift):
     result = ""
     for letter in text:
